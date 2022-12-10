07098f9a4ce9258203da13c6c014dcc30f34433a
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-12-10 09:49:04 -0700

    FIniSH 2021 6b
    had to use a map instead of ending up with an array of length 1754000560399

--- cmd/2021/6/main.go
+++ cmd/2021/6/main.go
@@ -9,28 +9,27 @@ import (
 )
 
 func main() {
-	// f := "input.txt"
-	f := "test.txt"
+	f := "input.txt"
+	// f := "test.txt"
 
 	_, filename, _, _ := runtime.Caller(0)
-	fish := aoc.ParseSplit(strings.TrimSpace(aoc.ReadFile(f, filename)), ",")
-	// lmfao
-	for days := 0; days < 80; days++ {
-		newFish := 0
-		for i := 0; i < len(fish); i++ {
-			fish[i]--
-			if fish[i] == -1 {
-				fish[i] = 6
-				newFish++
-			}
+	fish := map[int]int{}
+	fish[6] = 0
+	for _, v := range aoc.ParseSplit(strings.TrimSpace(aoc.ReadFile(f, filename)), ",") {
+		fish[v]++
+	}
+	for days := 0; days < 256; days++ {
+		newFish := fish[0]
+		for i := 0; i < 8; i++ {
+			fish[i] = fish[i+1]
 		}
-		fmt.Println(len(fish), newFish)
-		for i := 0; i < newFish; i++ {
-			fish = append(fish, 8)
-		}
-		fmt.Println(len(fish))
-		fmt.Println("day:", days)
+		fish[6] += newFish
+		fish[8] = newFish
 		fmt.Println(fish)
 	}
-	fmt.Println(len(fish))
+	total := 0
+	for _, v := range fish {
+		total += v
+	}
+	fmt.Println(total)
 }
