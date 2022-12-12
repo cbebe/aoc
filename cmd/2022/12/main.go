4b68c9609aaeeed6d69d37da271d88876bac6231
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-12-12 00:59:17 -0700

    finish 2022 12a

---
+++ cmd/2022/12/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"runtime"
+
+	"github.com/cbebe/aoc"
+)
+
+type Grid [][]Point
+
+type Point struct {
+	Value rune
+	X     int
+	Y     int
+}
+
+var visited aoc.Set[Point]
+var distance map[Point]int
+var grid Grid
+
+var pq aoc.PriorityQueue[Point]
+
+var start Point
+var end Point
+
+func main() {
+	f := "input.txt"
+	// f := "test.txt"
+	visited = make(aoc.Set[Point])
+	distance = make(map[Point]int)
+	pq = make(aoc.PriorityQueue[Point], 0)
+	grid = make(Grid, 0)
+
+	_, filename, _, _ := runtime.Caller(0)
+	for i, v := range aoc.Lines(f, filename) {
+		if v == "" {
+			continue
+		}
+		row := make([]Point, 0, len(v))
+		for j, w := range v {
+			point := Point{Value: w, X: j, Y: i}
+			distance[point] = math.MaxInt
+			if w == 'S' {
+				start = point
+				distance[point] = 0
+			} else if w == 'E' {
+				end = point
+			}
+			row = append(row, point)
+		}
+		grid = append(grid, row)
+	}
+	// fmt.Println(grid)
+	dijkstra()
+}
+
+func dijkstra() {
+	pq.Push(&aoc.PQItem[Point]{Value: start, Priority: 0})
+	for len(pq) != 0 {
+		node := pq.Min()
+		point, dist := node.Value, node.Priority
+		fmt.Println(point)
+		if point == end {
+			fmt.Println(dist)
+			return
+		}
+		if visited.Has(point) {
+			continue
+		}
+		visited.Add(point)
+		// fmt.Println(len(*distance))
+		// fmt.Println(string(point.Value), dist, point.X, point.Y)
+		for _, p := range point.getChildren() {
+			d, _ := aoc.MinMax(dist+1, distance[p])
+			distance[p] = d
+			pq.Push(&aoc.PQItem[Point]{Value: p, Priority: d})
+		}
+		// if node.Priority != 0 {
+		// 	fmt.Print(string(node.Value.Value))
+		// }
+	}
+	log.Fatalf("goal not found")
+}
+
+func (g Grid) getCell(x, y int) *Point {
+	if len(g) > 0 && y >= 0 && y < len(g) && x >= 0 && x < len(g[0]) {
+		return &g[y][x]
+	}
+	return nil
+}
+
+func val(r rune) int {
+	if r == 'S' {
+		return 96
+	} else if r == 'E' {
+		return 123
+	}
+	return int(r)
+}
+
+func (p Point) isNext(other Point) bool {
+	return val(other.Value) <= val(p.Value) || val(other.Value) == val(p.Value)+1
+}
+
+func (p Point) getChildren() []Point {
+	children := []Point{}
+	for _, other := range []*Point{grid.getCell(p.X-1, p.Y), grid.getCell(p.X+1, p.Y), grid.getCell(p.X, p.Y-1), grid.getCell(p.X, p.Y+1)} {
+		if other != nil && p.isNext(*other) {
+			children = append(children, *other)
+		}
+	}
+	return children
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("%s (%d-%d)", string(p.Value), p.X, p.Y)
+}
--- ds.go
+++ ds.go
@@ -1,5 +1,9 @@
 package aoc
 
+import (
+	"container/heap"
+)
+
 type Stack[T any] []T
 
 func (s *Stack[T]) Push(item T) {
@@ -107,4 +111,62 @@ func (s Set[T]) First() T {
 	}
 
 	return val
+}
+
+type PQItem[T any] struct {
+	Value    T
+	Priority int
+	index    int
+}
+
+type PriorityQueue[T any] []*PQItem[T]
+
+func (pq PriorityQueue[T]) Len() int { return len(pq) }
+
+func (pq PriorityQueue[T]) Less(i, j int) bool {
+	return (*pq[i]).Priority < (*pq[j]).Priority
+}
+
+func (pq *PriorityQueue[T]) Min() *PQItem[T] {
+	return heap.Remove(pq, 0).(*PQItem[T])
+}
+
+func (pq PriorityQueue[T]) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue[T]) Push(x any) {
+	n := len(*pq)
+	item := x.(*PQItem[T])
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue[T]) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue[T]) Update(item *PQItem[T], value T, priority int) {
+	item.Value = value
+	item.Priority = priority
+	heap.Fix(pq, item.index)
+}
+
+func PriorityQueueFromSlice[T any](items []PQItem[T]) *PriorityQueue[T] {
+	pq := make(PriorityQueue[T], 0, len(items))
+	for _, v := range items {
+		p := v
+		pq = append(pq, &p)
+	}
+	heap.Init(&pq)
+	return &pq
 }
